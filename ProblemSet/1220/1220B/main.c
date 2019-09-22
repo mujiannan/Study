7646b5766900b7b649273c81419f8845b4fb6337
mujiannan <littlesand@outlook.com>
2019-09-22 13:59:14 +0800

    Correct 1220B on 32-bit compute

--- ProblemSet/1220/1220B/main.c
+++ ProblemSet/1220/1220B/main.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 /*10的6次方的方阵，空间太大了，需要想些办法*/
 int main(){
-	extern unsigned long sqr();
+	extern long long sqrt();
 	int n;
 	scanf("%d",&n);
-	unsigned long source[n][n];
+	long long source[n][n];
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
-			scanf("%lu",&source[i][j]);
+			scanf("%llu",&source[i][j]);
 			/*printf("i=%d,j=%d,value=%d,value=%llu\n",i,j,source[i][j]);*/
 			
 		}
 	}
-	unsigned long result[n];
+	long long result[n];
 	for(int i=1;i<n-1;i++){
-		/*printf("sqrt(%lu*%lu/%lu)=%lu\n",source[0][i+1],source[i][i+1],source[0][i],sqr(source[0][i+1]*source[i][i+1]/source[0][i]));*/
-		result[i+1]=sqr(source[0][i+1]*source[i][i+1]/source[0][i]);
+		/*printf("sqrt(%lu*%lu/%lu)=%lu\n",source[0][i+1],source[i][i+1],source[0][i],sqrt(source[0][i+1]*source[i][i+1]/source[0][i]));*/
+		result[i+1]=sqrt(source[0][i+1]*source[i][i+1]/source[0][i]);
 	}
 	result[0]=source[0][n-1]/result[n-1];
 	result[1]=source[0][1]/result[0];
 	for(int i=0;i<n;i++){
-		printf("%lu ",result[i]);
+		printf("%llu ",result[i]);
 	}
 	return 0;
 }
-unsigned long sqr(unsigned long x){
-	for(int i=0;i<=x;i++){
+long long sqrt(long long x){
+	for(long long i=0;i<=x;i++){
 		if(i*i==x){
 			return i;
 		}
--- ProblemSet/1220/1220B/test.c
+++ ProblemSet/1220/1220B/test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
+	printf("size of double:%d\n",sizeof(double));
+	printf("size of long double:%d\n",sizeof(long double));
+	printf("size of long:%d\n",sizeof(long));
 	unsigned long x=1;
 	for(int i=0;i<x;i++){
 		if(i*i==x){
